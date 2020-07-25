c573b4f8fb6330f6c457814ec98705bd1f65d211
me_media <vgoogle@i.ua>
2020-07-26 02:08:38 +0300

    console c#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Environment.MachineName;
+            Console.WriteLine("Hello World! " + name);
+        }
+    }
+}
